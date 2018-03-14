77a971ee246799f28b9ccda5ad70be901fb632f1
Kevin Stephens <stepkevi@gmail.com>
2018-03-14 14:09:40 -0700

    Added FCBuilder.Execute() extension method as a shortcut for .Finalize().Execute() (suggested by Reddit user rekabmot)

---
+++ Assets/Plugins/FluentCoroutines/Extensions/FCBuilderExtensions.cs
@@ -0,0 +1,20 @@
+namespace FluentCoroutines.Extensions
+{
+    public static class FCBuilderExtensions
+    {
+        /// <summary>
+        /// Creates a finalized FluentCoroutine and executes it.
+        /// </summary>
+        /// <returns>Finalized form of the FluentCoroutine.</returns>
+        /// <remarks>
+        /// This method implicitly calls <see cref="Finalize"/> and returns the result of that call.
+        /// In other words, this method allocates memory for a new instance of FluentCoroutine every
+        /// time it is called. If you intend to reuse the FluentCoroutine, subsequent calls to Execute
+        /// should be made on the returned FluentCoroutine rather than the FCBuilder.
+        /// </remarks>
+        public static FluentCoroutine Execute(this FCBuilder builder)
+        {
+            return FluentCoroutine.Finalize(builder).Execute();
+        }
+    }
+}
