3e1c700d5efc8d9451f27ae5020f3d37a351d9c6
Kevin Stephens <stepkevi@gmail.com>
2018-03-14 14:32:30 -0700

    Renamed FCMonoBehaviourExtensions to MonoBehaviourExtensions, moved the file location so that folders now correspond with namespaces

--- Assets/Plugins/FluentCoroutines/MonoBehaviourExtensions.cs
+++ Assets/Plugins/FluentCoroutines/MonoBehaviourExtensions.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace FluentCoroutines
 {
-    public static class FCMonoBehaviourExtensions
+    public static class MonoBehaviourExtensions
     {
         public static IFCInitializer FluentCoroutine(this MonoBehaviour monoBehaviour)
         {
